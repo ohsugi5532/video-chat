a014d920a75bcfbf2965e5575d9ecea08263e45d
ohsugi5532 <t.ohsugi5532@gmail.com>
2021-05-13 16:24:34 +0900

    Add meeting page

--- frontend/src/pages/Meeting/index.tsx
+++ frontend/src/pages/Meeting/index.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React from 'react';
+import {
+  VideoTileGrid,
+  UserActivityProvider
+} from 'amazon-chime-sdk-component-library-react';
 
 const Meeting: React.FC = () => {
   return (
-    <div>
-      Meeting Now!
-    </div>
+    <UserActivityProvider>
+      <VideoTileGrid
+        className="videos"
+      />
+    </UserActivityProvider>
   )
 }
 
